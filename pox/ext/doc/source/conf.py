cf8f953614870a67db86d8388386b47968c0983e
Janos Czentye <czentye@tmit.bme.hu>
2015-05-11 15:54:22 +0200

    Remove source link from html files

--- pox/ext/doc/source/conf.py
+++ pox/ext/doc/source/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../../..'))  # pox dir to handle lib/...
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', ]
+              'sphinx.ext.ifconfig', ] #  'sphinx.ext.viewcode'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
