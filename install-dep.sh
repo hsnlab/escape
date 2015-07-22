01cc4cb2b3976e07a92bb3d4cffc0c081897301a
Janos Czentye <czentye@tmit.bme.hu>
2015-07-22 20:09:07 +0200

    Add networkx dependency

--- install-dep.sh
+++ install-dep.sh
@@ -5,4 +5,4 @@ sudo apt-get update
 # Install dependencies
 sudo apt-get -y install libxml2-dev libxslt1-dev zlib1g-dev \
 python-pip python-libxml2 python-libxslt1 python-lxml python-paramiko
-sudo pip install requests jinja2 ncclient lxml
+sudo pip install requests jinja2 ncclient lxml networkx
