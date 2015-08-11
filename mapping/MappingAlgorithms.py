4177aae8723559bfa59d2303ec35d4f8e1babc29
Janos Czentye <czentye@tmit.bme.hu>
2015-08-11 13:11:47 +0200

    Fix import issue to support older python versions

--- mapping/MappingAlgorithms.py
+++ mapping/MappingAlgorithms.py
@@ -33,11 +33,12 @@ import networkx as nx
 try:
   from escape.util.nffg import NFFG, generate_dynamic_fallback_nffg
 except ImportError:
-  import sys
-  import os
+  import sys, os, inspect
 
-  sys.path.insert(0, os.path.join(
-    os.path.abspath(os.path.dirname(__file__) + "/.."), "pox/ext/escape/util/"))
+  sys.path.insert(0, os.path.join(os.path.abspath(os.path.realpath(
+    os.path.abspath(
+      os.path.split(inspect.getfile(inspect.currentframe()))[0])) + "/.."),
+                                  "pox/ext/escape/util/"))
   from nffg import NFFG, generate_dynamic_fallback_nffg
 from Alg1_Core import CoreAlgorithm
 import UnifyExceptionTypes as uet
