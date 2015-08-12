175a20dd8213561173c498eb2839358b507fabb0
Balazs Sonkoly <sonkoly@tmit.bme.hu>
2015-08-12 07:57:30 -0700

    nfib_mgmt.py: add initialize function to load data to nfib

--- pox/ext/escape/orchest/nfib_mgmt.py
+++ pox/ext/escape/orchest/nfib_mgmt.py
@@ -497,3 +497,128 @@ class NFIBManager(object):
     :return: None
     """
     self.graph_db.delete_all()
+
+  def initialize (self):
+    """
+    Initialize NFIB with test data.
+    """
+    # start clean - all the existing info is removed from the DB
+    self.removeGraphDB()
+    
+    # add new high-level NF to the DB, all the information related to the NF should be given as a dict 
+    self.addNode({'label':'NF','node_id':'forwarder','type':'NA'})
+    self.addNode({'label':'NF','node_id':'compressor','type':'NA'})
+    self.addNode({'label':'NF','node_id':'decompressor','type':'NA'})
+    
+    log.debug("%s : high-level  NFs were added to the DB" % self.__class__.__name__)
+    
+    # generate a  decomposition for a high-level forwarder NF (in form of networkx)
+    G1 = networkx.DiGraph()
+    G1.add_path(['SAP1','simpleForwarder','SAP2'])
+    
+    # create node properties
+    for n in G1.nodes():
+      properties = {}
+      properties['node_id'] = n
+
+      if 'SAP'  in n:
+        properties['label'] = 'SAP'
+        properties['type'] = 'NA'
+      else:
+        properties['label'] = 'NF'
+        properties['type'] = 'click'  
+        properties['cpu'] = 10
+        properties['mem'] = 100
+        properties['storage'] = 100
+      G1.node[n]['properties'] = properties
+    
+    # create edge properties
+    properties = {}
+    properties['BW'] = 100
+    properties['src_port']=1
+    properties['dst_port']=1
+    G1.edge['SAP1']['simpleForwarder']['properties'] = properties
+    
+    properties1 = {}
+    properties1['BW'] = 100
+    properties1['src_port']=2
+    properties1['dst_port']=2
+    G1.edge['simpleForwarder']['SAP2']['properties'] = properties1 
+    
+    # generate a decomposition for a high-level compressor NF (in form of networkx)
+    G2 = networkx.DiGraph()
+    G2.add_path(['SAP3','headerCompressor','SAP4'])
+    
+    # create node properties
+    for n in G2.nodes():
+      properties = {}
+      properties['node_id'] = n
+      if 'SAP' in n:
+        properties['label'] = 'SAP'
+        properties['type'] = 'NA'
+      else:
+        properties['label'] = 'NF'
+        properties['type'] = 'click'
+        properties['cpu'] = 20
+        properties['mem'] = 200
+        properties['storage'] = 200
+      G2.node[n]['properties'] = properties
+    
+    # create edge properties 
+    properties3 = {}
+    properties3['BW'] = 200
+    properties3['src_port'] = 1
+    properties3['dst_port'] = 1
+    G2.edge['SAP3']['headerCompressor']['properties'] = properties3
+    
+    properties4 = {}
+    properties4['BW'] = 200
+    properties4['src_port'] = 2
+    properties4['dst_port'] =2
+    G2.edge['headerCompressor']['SAP4']['properties'] = properties4
+    
+    # generate a decomposition for a high-level decompressor NF (in form of networkx)
+    G3 = networkx.DiGraph()
+    G3.add_path(['SAP5','headerDecompressor','SAP6'])
+    
+    # create node properties
+    for n in G3.nodes():
+      properties = {}
+      properties['node_id'] = n
+      if 'SAP' in n:
+        properties['label'] = 'SAP'
+        properties['type'] = 'NA'
+      else:
+        properties['label'] = 'NF'
+        properties['type'] = 'click'
+        properties['cpu'] = 30
+        properties['mem'] = 300
+        properties['storage'] = 300
+      G3.node[n]['properties'] = properties
+    
+    # create edge properties
+    properties5 = {}
+    properties5['BW'] = 300
+    properties5['src_port'] = 1
+    properties5['dst_port'] = 1
+    G3.edge['SAP5']['headerDecompressor']['properties'] = properties5
+    
+    properties6 ={}
+    properties6['BW'] = 300
+    properties6['src_port'] = 2
+    properties6['dst_port'] = 2
+    G3.edge['headerDecompressor']['SAP6']['properties'] = properties6
+    
+    # required elementary NFs should be added first to the DB
+    self.addClickNF({'label':'NF', 'node_id':'Queue','type:':'click'})
+    self.addClickNF({'label':'NF','node_id':'Classifier','type':'click'})
+    self.addClickNF({'label':'NF','node_id':'Counter', 'type':'click'})
+    self.addClickNF({'label':'NF', 'node_id':'RFC2507Comp', 'type':'click'})
+    self.addClickNF({'label':'NF','node_id':'RFC2507Decomp','type':'click'})
+    
+    # the NF decompositions are added to the DB
+    self.addDecomp('forwarder', 'G1', G1)
+    self.addDecomp('compressor', 'G2', G2)
+    self.addDecomp('decompressor','G3', G3)
+    
+    log.debug("%s : NF decompositions were added to the DB" % self.__class__.__name__)
--- pox/ext/escape/orchest/testNFIBManager.py
+++ pox/ext/escape/orchest/testNFIBManager.py
@@ -1,11 +1,25 @@
+# Copyright 2015 Sahel Sahhaf <sahel.sahhaf@intec.ugent.be>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
 Test different interfaces of the NFIB
 """
 
 import networkx
-from ros_orchestration import NFIBManager
-from util.nffg_elements import *
-from util.nffg import *
+from escape.orchest.nfib_mgmt import NFIBManager
+from escape.util.nffg_elements import *
+from escape.util.nffg import *
+
 
 def testNFIB ():
  
