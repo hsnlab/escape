4893de4d4f8aef68c4641899e375d1c7794eafb0
Balazs Sonkoly <sonkoly@tmit.bme.hu>
2015-10-05 03:16:33 -0700

    adapt/adapters.py: bugfix in _delete_flowrules function of InternalDomainManager, now topoAdapter is used to retrieve resource info (instead of controlAdapter)

--- pox/ext/escape/adapt/managers.py
+++ pox/ext/escape/adapt/managers.py
@@ -261,7 +261,7 @@ class InternalDomainManager(AbstractDomainManager):
     """
     Delete all flowrules from the first (default) table of all infras.
     """
-    topo = self.controlAdapter.get_topology_resource()
+    topo = self.topoAdapter.get_topology_resource()
     # Iter through the container INFRAs in the given mapped NFFG part
     for infra in nffg_part.infras:
       if infra.infra_type not in (
