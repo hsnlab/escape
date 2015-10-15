4af660a0e3f3c91ecc391c291e3b769f8af36ca6
Balazs Nemeth <nem841425bal@gmail.com>
2015-10-15 05:37:42 -0700

    Add function which scans the parameter space scarcly. 	modified:   ParameterSearch.py

--- mapping/ParameterSearch.py
+++ mapping/ParameterSearch.py
@@ -30,6 +30,9 @@ helpmsg = """ParameterSearch.py usage:
    --seeds=a,b,..  The sequence of seeds which should be used for the SC 
                    sequence generation. The length of the list determines how 
                    many should be executed.
+   --param_number=i    Defines which parameter space point from a specific set 
+                       shall be tested with all test sequences. If param_number
+                       is given, --threads is ignored.
 """
 
 def oneLineInParameterSpace(factor1, factor2, factor3, outfile, other=[]):
@@ -62,7 +65,7 @@ class TestSequence(threading.Thread):
     semaphore.acquire()
     with open(outfile, "w") as f:
       f.write(self.threadname+" seed: "+s)
-    """
+
     oneLineInParameterSpace("--res_factor=", "--bw_factor=", "--lat_factor=", 
                             outfile, ["--request_seed="+s])
     oneLineInParameterSpace("--res_factor=", "--lat_factor=", "--bw_factor=", 
@@ -79,7 +82,7 @@ class TestSequence(threading.Thread):
     oneLineInParameterSpace("--lat_factor=", "--bw_factor=", "--res_factor=", 
                             outfile, ["--vnf_sharing=0.3",
                                       "--request_seed="+s])
-    """
+
     oneLineInParameterSpace("--res_factor=", "--bw_factor=", "--lat_factor=", 
                             outfile, ["--vnf_sharing=0.2",
                                       "--multiple_scs",
@@ -97,12 +100,26 @@ class TestSequence(threading.Thread):
                                       "--request_seed="+s])
     semaphore.release()
 
+def runAllTestSequences(par1, par2, par3, seeds, outfile):
+  for s in seeds:
+    basecmd = "python StressTest.py --request_seed="+str(s)
+    comm = " ".join((basecmd,"--res_factor="+str(par1), 
+                     "--bw_factor="+str(par2), "--lat_factor="+str(par3)))
+    for seqtype in ["", " --vnf_sharing=0.2",
+                    " --vnf_sharing=0.2 --multiple_scs --max_sc_count=3"]:
+      typed_comm = comm + seqtype
+      with open(outfile, "a") as f:
+        f.write("\n Command under execution:\n %s"%typed_comm)
+      typed_comm += " -o " + outfile
+      os.system(typed_comm)
+
 if __name__ == '__main__':
   try: 
-    opts, args = getopt.getopt(sys.argv[1:],"ho:", ["threads=", "seeds="])
+    opts, args = getopt.getopt(sys.argv[1:],"ho:", ["threads=", "seeds=", "param_number="])
     baseoutfile = "paramsearch"
     threads = 1
     seedlist = [0,1]
+    param_number = None
     for opt, arg in opts:
       if opt == "-h":
         print helpmsg
@@ -113,15 +130,36 @@ if __name__ == '__main__':
         threads = int(arg)
       elif opt == "--seeds":
         seedlist = map(int, arg.split(","))
+      elif opt == "--param_number":
+        param_number = int(arg)
   except Exception as e:
     print traceback.format_exc()
     print helpmsg
     sys.exit()
-  global semaphore
-  launched_threads = 0
-  semaphore = threading.Semaphore(threads)
-  for s in seedlist:
-    launched_threads += 1
-    seq = TestSequence("Thread-"+str(launched_threads), 
-                       baseoutfile, s)
-    seq.start()
+  if param_number is not None:
+    step = 0.375
+    i = 0.0
+    j = 0.0
+    cnt = 0
+    while i <= 3.0:
+      while j <= 3.0 - i:
+        if cnt == param_number:
+          k = 3.0-i-j
+          print i,j,k
+          runAllTestSequences(i, j, k, seedlist, 
+          "-".join((baseoutfile,str(i),str(j),str(k)))+".out")
+          sys.exit()
+        else:
+          j += step
+          cnt += 1
+      i += step
+      j = 0.0
+  else:
+    global semaphore
+    launched_threads = 0
+    semaphore = threading.Semaphore(threads)
+    for s in seedlist:
+      launched_threads += 1
+      seq = TestSequence("Thread-"+str(launched_threads), 
+                         baseoutfile, s)
+      seq.start()
