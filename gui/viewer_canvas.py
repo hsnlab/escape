cae0d215590ce485b85e5dfbd3f846fcd02f699b
Janos Czentye <czentye@tmit.bme.hu>
2016-03-09 15:01:39 +0100

    Fix dynamic NFFG import in gui related files Add Virtualizer conversion to GUI

--- gui/viewer_canvas.py
+++ gui/viewer_canvas.py
@@ -1,52 +1,55 @@
 import networkx as nx
-from tokens import CustomNodeToken, CustomEdgeToken
 import sys
-import threading
+from Tkconstants import END
+
+from tokens import CustomNodeToken, CustomEdgeToken
 
 try:
-    # Python 3
-    import tkinter as tk
-    import tkinter.messagebox as tkm
-    import tkinter.simpledialog as tkd
+  # Python 3
+  import tkinter as tk
+  import tkinter.messagebox as tkm
+  import tkinter.simpledialog as tkd
 except ImportError:
-    # Python 2
-    import Tkinter as tk
-    import tkMessageBox as tkm
-    import tkSimpleDialog as tkd
+  # Python 2
+  import Tkinter as tk
+  import tkMessageBox as tkm
+  import tkSimpleDialog as tkd
 
 from networkx_viewer import *
 
-class SetterDialog( tkd.Dialog):
 
+class SetterDialog(tkd.Dialog):
   def body (self, master):
-    tk.Label (master, text="URL: ").grid(row=0);
-    self.e1 = tk.Entry(master)
+    tk.Label(master, text="URL: ").grid(row=0)
+    self.e1 = tk.Entry(master, width=40)
+    self.e1.insert(END, "http://localhost:8888/escape/get-config")
     self.e1.grid(row=0, column=1)
     return self.e1
 
-  def apply(self):
+  def apply (self):
     self.result = self.e1.get()
 
-  def get(self):
+  def get (self):
     return self.result
 
-class CustomViewerCanvas( ViewerApp):
 
-  def __init__(self, graph, **kwargs):
+class CustomViewerCanvas(ViewerApp):
+  def __init__ (self, graph, **kwargs):
     self.__kwargs = kwargs
-    ViewerApp.__init__(self, graph, NodeTokenClass=CustomNodeToken, EdgeTokenClass=CustomEdgeToken, **kwargs)
+    ViewerApp.__init__(self, graph, NodeTokenClass=CustomNodeToken,
+                       EdgeTokenClass=CustomEdgeToken, **kwargs)
     self.url = None
 
-    #Building menu
+    # Building menu
     tools = tk.Menu(self.menubar, tearoff=0)
     tools.add_command(label="Set URL", command=self.setURL)
     self.menubar.add_cascade(label="Tools", menu=tools)
 
-  def setURL(self):
+  def setURL (self):
     self.url = SetterDialog(self).get()
 
-  def refresh(self):
-    #search for removable nodes; if found, then redraw whole graph
+  def refresh (self):
+    # search for removable nodes; if found, then redraw whole graph
     iteratorD = self.canvas.dispG.nodes_iter()
     try:
       for k in self.canvas.dispG.nodes():
@@ -59,7 +62,7 @@ class CustomViewerCanvas( ViewerApp):
           for n in self.canvas.dispG.nodes():
             t_id = self.canvas.dispG.node[n]['token_id']
             self.canvas.dispG.node[n]['token'].delete('all')
-            del(self.canvas.dispG.node[n])
+            del (self.canvas.dispG.node[n])
           self.canvas.dispG.clear()
           self.canvas.refresh()
           self.canvas._graph_changed()
@@ -67,17 +70,21 @@ class CustomViewerCanvas( ViewerApp):
       for e in self.canvas.dispG.edges():
         u_dataG_id = self.canvas.dispG.node[e[0]]['dataG_id']
         v_dataG_id = self.canvas.dispG.node[e[1]]['dataG_id']
-        
-        if not self.canvas.dataG.has_edge(u_dataG_id,v_dataG_id) and not self.canvas.dataG.has_edge(v_dataG_id,u_dataG_id):
-          #TODO needed to add switched edge to-from because a particular link would've been removed. Need to investigate further
+
+        if not self.canvas.dataG.has_edge(u_dataG_id,
+                                          v_dataG_id) and not \
+           self.canvas.dataG.has_edge(
+             v_dataG_id, u_dataG_id):
+          # TODO needed to add switched edge to-from because a particular
+          # link would've been removed. Need to investigate further
           for id in self.canvas.dispG.edge[e[0]][e[1]]:
             t_id = self.canvas.dispG.edge[e[0]][e[1]][id]['token_id']
             self.canvas.delete(t_id)
           del (self.canvas.dispG.edge[e[0]][e[1]])
     except:
-      print "%s Unhandled exception: %s" % (__name__,sys.exc_info()[0])
+      print "%s Unhandled exception: %s" % (__name__, sys.exc_info()[0])
 
-    #need to add new/updated node/edge to dispG
+    # need to add new/updated node/edge to dispG
     iterator = self.canvas.dataG.nodes_iter()
     for n in iterator:
       iteratorD = self.canvas.dispG.nodes_iter()
@@ -87,13 +94,14 @@ class CustomViewerCanvas( ViewerApp):
           found = True
           break
       if not found:
-        t = CustomNodeToken(self.canvas,self.canvas.dataG.node[n],n)
+        t = CustomNodeToken(self.canvas, self.canvas.dataG.node[n], n)
         id = self.canvas.create_window(50, 50, window=t, anchor=tk.CENTER,
-                         tags='node')
-        self.canvas.dispG.add_node(id,{'dataG_id' : n,'token' : t, 'token_id' : id})
+                                       tags='node')
+        self.canvas.dispG.add_node(id,
+                                   {'dataG_id': n, 'token': t, 'token_id': id})
     return
-    #TODO code below is copied from parent class, currently unused
-    #going through possible new edges
+    # TODO code below is copied from parent class, currently unused
+    # going through possible new edges
     iterator = self.canvas.dataG.edges_iter()
     for e in iterator:
       u = e[0]
@@ -105,8 +113,8 @@ class CustomViewerCanvas( ViewerApp):
         to_disp = self.canvas._find_disp_node(e[1])
       except NodeFiltered:
         break
-      if not self.canvas.dispG.has_edge(frm_disp,to_disp):
-        #TODO create EdgeToken
+      if not self.canvas.dispG.has_edge(frm_disp, to_disp):
+        # TODO create EdgeToken
         if isinstance(self.canvas.dataG, nx.MultiGraph):
           edges = self.canvas.dataG.edge[u][v]
         elif isinstance(self.canvas.dataG, nx.Graph):
@@ -120,22 +128,24 @@ class CustomViewerCanvas( ViewerApp):
         for key, data in edges.items():
           token = CustomEdgeToken(data)
           if isinstance(self.canvas.dataG, nx.MultiGraph):
-            dataG_id = (u,v,key)
+            dataG_id = (u, v, key)
           elif isinstance(self.canvas.dataG, nx.Graph):
-            dataG_id = (u,v)
-          self.canvas.dispG.add_edge(frm_disp, to_disp, key, {'dataG_id': dataG_id,
-                                    'dispG_frm': frm_disp,
-                                    'token': token,
-                                    'm': m})
-          x1,y1 = self.canvas._node_center(frm_disp)
-          x2,y2 = self.canvas._node_center(to_disp)
-          xa,ya = self.canvas._spline_center(x1,y1,x2,y2,m)
+            dataG_id = (u, v)
+          self.canvas.dispG.add_edge(frm_disp, to_disp, key,
+                                     {'dataG_id': dataG_id,
+                                      'dispG_frm': frm_disp,
+                                      'token': token,
+                                      'm': m})
+          x1, y1 = self.canvas._node_center(frm_disp)
+          x2, y2 = self.canvas._node_center(to_disp)
+          xa, ya = self.canvas._spline_center(x1, y1, x2, y2, m)
 
           cfg = token.render()
-          l = self.canvas.create_line(x1,y1,xa,ya,x2,y2, tags='edge', smooth=True, **cfg)
+          l = self.canvas.create_line(x1, y1, xa, ya, x2, y2, tags='edge',
+                                      smooth=True, **cfg)
           self.canvas.dispG[frm_disp][to_disp][key]['token_id'] = l
 
           if m > 0:
-            m = -m # Flip sides
+            m = -m  # Flip sides
           else:
-            m = -(m+m)  # Go next increment out
+            m = -(m + m)  # Go next increment out
--- gui/viewer_thread.py
+++ gui/viewer_thread.py
@@ -1,81 +1,108 @@
-import threading
-import viewer_canvas as vCanvas
+import logging
 import networkx as nx
-import sys
+import threading
+
 import http_client as http
-import nffg
-#from multiprocessing import Process
+import viewer_canvas as vCanvas
+
+try:
+  import nffg as ng
+except ImportError:
+  import sys, os
+
+  sys.path.extend((os.path.abspath("../pox/ext/escape/nffg_lib"),
+                   os.path.abspath("../pox/ext/escape/util"),
+                   os.path.abspath("../pox/ext/escape/unify_virtualizer"),
+                   ))
+  from nffg import NFFG
+  from conversion import NFFGConverter
+
+
+# from multiprocessing import Process
 
 class ViewerThread(threading.Thread):
-
-  def __init__ (self, viewer_type='get', graph = None, address = "127.0.0.1:80"):
-    super(ViewerThread,self).__init__()
+  def __init__ (self, viewer_type='get', graph=None, address="127.0.0.1:80"):
+    super(ViewerThread, self).__init__()
     '''
     Constructor
     Takes a networkx object and launches a GUI to view the parameters
     of the network using networkx_viewer
     '''
-    #handling other parameters
+    # handling other parameters
     self.__http_client = None
     self.__timer = None
     graph = None
-    if graph == None:
+    if graph is None:
       graph = nx.MultiDiGraph()
-      graph.add_node(1,{'name':'Set URL'})
-      graph.add_node(2,{'name':'to view NFFG'})
-      graph.add_edge(1,2)
+      graph.add_node(1, {'name': 'Set URL'})
+      graph.add_node(2, {'name': 'to view NFFG'})
+      graph.add_edge(1, 2)
 
     self.__network = graph
     if viewer_type == 'get':
       self.__timer = threading.Timer(1, self.timerHandler)
       self.__http_client = http.GetClient()
     self.__update = False
+    self._converter = NFFGConverter(logger=logging.getLogger(__name__))
 
-  def timerHandler(self):
+  def timerHandler (self):
     new_nffg = None
-    if not self.__viewer.url == None:
+    if self.__viewer.url is not None:
       n = self.__http_client.getNFFG(self.__viewer.url)
-      if not n == None:
-        new_nffg = nffg.NFFG.parse(n)
-      if not new_nffg == None:
+      if n is not None:
+        if str(n).startswith("<?xml"):
+          new_nffg = self._converter.parse_from_Virtualizer(n)
+        else:
+          new_nffg = NFFG.parse(n)
+      if new_nffg is not None:
         new_nffg.merge_duplicated_links()
-        #Add new nodes from new NFFG - Must add before remove
-        #self.__network.add_nodes_from(new_nffg.network.nodes())
+        # Add new nodes from new NFFG - Must add before remove
+        # self.__network.add_nodes_from(new_nffg.network.nodes())
         for node in new_nffg.network.nodes():
           self.__network.add_node(node)
-          self.__network.node[node].update( new_nffg.network.node[node].persist())
-          self.__network.node[node].update( {"element type" : new_nffg.network.node[node].type})
-        #Add new edges from new NFFG
-        for u,v in new_nffg.network.edges():
+          self.__network.node[node].update(
+            new_nffg.network.node[node].persist())
+          self.__network.node[node].update(
+            {"element type": new_nffg.network.node[node].type})
+
+        # Add new edges from new NFFG
+        for u, v in new_nffg.network.edges():
           for i in new_nffg.network.edge[u][v]:
-            #checking for multiple links between the two nodes
+            # checking for multiple links between the two nodes
             istr = i
-            if self.__network.has_edge(u,v) and istr in self.__network.edge[u][v]:
-              #TODO link already added, only update
+            if self.__network.has_edge(u, v) and istr in self.__network.edge[u][
+              v]:
+              # TODO link already added, only update
               pass
             else:
-              self.__network.add_edge(u,v,istr)
-            self.__network.edge[u][v][istr].update(new_nffg.network.edge[u][v][i].persist())
-            self.__network.edge[u][v][istr].update( {"element type" : new_nffg.network.edge[u][v][i].type})
+              self.__network.add_edge(u, v, istr)
+            self.__network.edge[u][v][istr].update(
+              new_nffg.network.edge[u][v][i].persist())
+            self.__network.edge[u][v][istr].update(
+              {"element type": new_nffg.network.edge[u][v][i].type})
 
-        #Remove nodes not present in new NFFG
+        # Remove nodes not present in new NFFG
         for node in self.__network.nodes():
           if not new_nffg.network.has_node(node):
             self.__network.remove_node(node)
           else:
-            #updating dict of the node
-            self.__network.node[node].update( new_nffg.network.node[node].persist())
-            self.__network.node[node].update( {"element type" : new_nffg.network.node[node].type})
+            # updating dict of the node
+            self.__network.node[node].update(
+              new_nffg.network.node[node].persist())
+            self.__network.node[node].update(
+              {"element type": new_nffg.network.node[node].type})
 
-        #Remove edges not present in new NFFG
-        for u,v in self.__network.edges():
+        # Remove edges not present in new NFFG
+        for u, v in self.__network.edges():
           if not new_nffg.network.has_edge(u, v):
             self.__network.remove_edge(u, v)
           else:
-            #updating dict of the edge
+            # updating dict of the edge
             for i in new_nffg.network.edge[u][v]:
-              self.__network.edge[u][v][i].update(new_nffg.network.edge[u][v][i].persist())
-              self.__network.edge[u][v][i].update( {"element type" : new_nffg.network.edge[u][v][i].type})
+              self.__network.edge[u][v][i].update(
+                new_nffg.network.edge[u][v][i].persist())
+              self.__network.edge[u][v][i].update(
+                {"element type": new_nffg.network.edge[u][v][i].type})
 
         self.__viewer.canvas.dataG = self.__network
         self.__viewer.refresh()
@@ -83,8 +110,8 @@ class ViewerThread(threading.Thread):
     self.__timer = threading.Timer(1, self.timerHandler)
     self.__timer.start()
 
-  def run(self):
-    #Modified Node renderer
+  def run (self):
+    # Modified Node renderer
     if self.__timer:
       self.__timer.start()
       pass
@@ -92,12 +119,11 @@ class ViewerThread(threading.Thread):
     should_continue = True
     try:
       while should_continue:
-        #self.__viewer.update_idletasks()
-        #self.__viewer.update()
+        # self.__viewer.update_idletasks()
+        # self.__viewer.update()
         self.__viewer.mainloop()
         break
     except:
       self.__timer.cancel()
     self.__timer.cancel()
     return
-
