4d56ca7f30b4697278afe1a6fdfd3fe29494d96d
Balazs Nemeth <nem841425bal@gmail.com>
2016-03-23 10:42:43 -0700

    Add few more tests and decrease test lengths.

--- mapping/night_test.py
+++ mapping/night_test.py
@@ -14,44 +14,54 @@ class Test(threading.Thread):
         semaphore.release()
 
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=7 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=7 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=8 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=8 --bt_br_factor=2").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=2 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=2 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=300 -t --batch_length=2 --bt_limit=12 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=decent --seed_start=0 --seed_end=220 -t --batch_length=2 --bt_limit=12 --bt_br_factor=2").start()
 
 #=====================================
 
-Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=300 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=sharing --seed_start=0 --seed_end=220 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=sharing --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
 
-Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=8 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=sharing --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=8 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=sharing --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+
+#=====================================
+
+Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=220 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
+
+Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+
+Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=8 --bt_br_factor=3").start()
+
+Test("python BatchTest-params.py --stress_type=agressive --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
 
 #==========================================
 
-Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=300 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=220 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=normal --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
 
 # ======================================
 
-Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=300 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=220 -t --batch_length=1 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
+Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=6 --bt_br_factor=3").start()
 
-Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=300 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
+Test("python BatchTest-params.py --stress_type=sc8decent --seed_start=0 --seed_end=220 -t --batch_length=4 --bt_limit=12 --bt_br_factor=2").start()
 
 #=========================================
