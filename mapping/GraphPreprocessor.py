e484484e14ba184b88748d142ea7325a6ea70bd6
Janos Czentye <czentye@tmit.bme.hu>
2016-04-05 15:08:15 +0200

    Update NFFG paths in mapping files

--- mapping/GraphPreprocessor.py
+++ mapping/GraphPreprocessor.py
@@ -31,7 +31,7 @@ try:
 except ImportError:
   import sys, os
   sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                  "../pox/ext/escape/nffg_lib/")))
+                                  "../escape/escape/nffg_lib/")))
   from nffg import NFFGToolBox
 
 
--- mapping/StressTest.py
+++ mapping/StressTest.py
@@ -34,7 +34,7 @@ try:
 except ImportError:
   import sys, os
   sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                  "../pox/ext/escape/nffg_lib/")))
+                                  "../escape/escape/nffg_lib/")))
   from nffg import NFFG
 
 def gen_seq():
