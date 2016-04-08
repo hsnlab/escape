3b57b358c25ca95dcd76ad0dc0f2a97268b47734
Janos Czentye <czentye@tmit.bme.hu>
2016-04-08 11:31:31 +0200

    Move dataplane related Manager Adapter to a separate file Fix config due to the moving

---
+++ escape/escape/adapt/dataplane.py
@@ -0,0 +1,166 @@
+import os
+
+from escape import CONFIG
+from escape.util.domain import *
+from escape.util.misc import run_cmd
+
+
+class DataplaneDomainManager(AbstractDomainManager):
+  """
+  Manager class to handle communication with internally emulated network.
+
+  .. note::
+    Uses :class:`InternalMininetAdapter` for managing the emulated network and
+    :class:`InternalPOXAdapter` for controlling the network.
+  """
+  # DomainManager name
+  name = "DATAPLANE"
+  # Default domain name
+  DEFAULT_DOMAIN_NAME = "DATAPLANE"
+  # Set the local manager status
+  IS_LOCAL_MANAGER = True
+
+  def __init__ (self, domain_name=DEFAULT_DOMAIN_NAME, *args, **kwargs):
+    """
+    Init
+    """
+    log.debug(
+      "Create DataplaneDomainManager with domain name: %s" % domain_name)
+    super(DataplaneDomainManager, self).__init__(domain_name=domain_name,
+                                                 *args, **kwargs)
+    # self.controlAdapter = None  # DomainAdapter for POX-InternalPOXAdapter
+    self.topoAdapter = None  # DomainAdapter for Mininet-InternalMininetAdapter
+
+  def init (self, configurator, **kwargs):
+    """
+    Initialize Internal domain manager.
+
+    :param configurator: component configurator for configuring adapters
+    :type configurator: :any:`ComponentConfigurator`
+    :param kwargs: optional parameters
+    :type kwargs: dict
+    :return: None
+    """
+    # Call abstract init to execute common operations
+    super(DataplaneDomainManager, self).init(configurator, **kwargs)
+    log.info("DomainManager for %s domain has been initialized!" %
+             self.domain_name)
+
+  def initiate_adapters (self, configurator):
+    """
+    Initiate adapters.
+
+    :param configurator: component configurator for configuring adapters
+    :type configurator: :any:`ComponentConfigurator`
+    :return: None
+    """
+    # Initiate Adapters
+    self.topoAdapter = configurator.load_component(
+      component_name=AbstractESCAPEAdapter.TYPE_TOPOLOGY,
+      parent=self._adapters_cfg)
+    # Init adapter for internal controller: POX
+    # self.controlAdapter = configurator.load_component(
+    #   component_name=AbstractESCAPEAdapter.TYPE_CONTROLLER,
+    #   parent=self._adapters_cfg)
+    log.debug("Set %s as the topology Adapter for %s" %
+              (self.topoAdapter.__class__.__name__, self.domain_name))
+    # self.controlAdapter.__class__.__name__),
+
+  def finit (self):
+    """
+    Stop polling and release dependent components.
+
+    :return: None
+    """
+    super(DataplaneDomainManager, self).finit()
+    # self.controlAdapter.finit()
+    self.topoAdapter.finit()
+
+  # @property
+  # def controller_name (self):
+  #   return self.controlAdapter.task_name
+
+  def install_nffg (self, nffg_part):
+    """
+    Install an :any:`NFFG` related to the dataplane domain.
+
+    :param nffg_part: NF-FG need to be deployed
+    :type nffg_part: :any:`NFFG`
+    :return: installation was success or not
+    :rtype: bool
+    """
+    log.info(">>> Install %s domain part..." % self.domain_name)
+    try:
+      # Mininet domain does not support NF migration directly -->
+      # Remove unnecessary and moved NFs first
+      # TODO
+      pass
+    except:
+      log.exception("Got exception during NFFG installation into: %s." %
+                    self.domain_name)
+      return False
+
+  def clear_domain (self):
+    """
+    Infrastructure Layer has already been stopped and probably cleared.
+
+    Skip cleanup process here.
+
+    :return: cleanup result
+    :rtype: bool
+    """
+    if not self.topoAdapter.check_domain_reachable():
+      # This would be the normal behaviour if ESCAPEv2 is shutting down -->
+      # Infrastructure layer has been cleared.
+      log.debug("%s domain has already been cleared!" % self.domain_name)
+      return True
+    # something went wrong ??
+    return False
+
+
+class DataplaneComputeCtrlAdapter(AbstractESCAPEAdapter):
+  """
+  Adapter class to handle communication with Mininet domain.
+
+  Implement VNF managing API using direct access to the
+  :class:`mininet.net.Mininet` object.
+  """
+  # Events raised by this class
+  _eventMixin_events = {DomainChangedEvent}
+  name = "COMPUTE"
+  type = AbstractESCAPEAdapter.TYPE_TOPOLOGY
+
+  def __init__ (self, **kwargs):
+    """
+    Init.
+
+    :param net: set pre-defined network (optional)
+    :type net: :class:`ESCAPENetworkBridge`
+    """
+    # Call base constructors directly to avoid super() and MRO traps
+    AbstractESCAPEAdapter.__init__(self, **kwargs)
+    log.debug("Init DataplaneComputeCtrlAdapter - type: %s" % self.type)
+
+  def check_domain_reachable (self):
+    """
+    Checker function for domain polling.
+
+    :return: the domain is detected or not
+    :rtype: bool
+    """
+    # Direct access to IL's Mininet wrapper <-- Internal Domain
+    return True
+
+  def get_topology_resource (self):
+    """
+    Return with the topology description as an :any:`NFFG`.
+
+    :return: the emulated topology description
+    :rtype: :any:`NFFG`
+    """
+    cmd_hwloc2nffg = os.path.normpath(os.path.join(
+      CONFIG.get_project_root_dir(), "hwloc2nffg/build/bin/hwloc2nffg"))
+    raw_data = run_cmd(cmd_hwloc2nffg)
+    topo = NFFG.parse(raw_data)
+    topo.duplicate_static_links()
+    return topo
