d74b701a5f0b79e891f84c8e3a9020b56e5a11e3
Balazs Nemeth <nem841425bal@gmail.com>
2016-05-14 09:50:15 -0700

    Tweak output processer script

--- mapping/calc_mapping_times.py
+++ mapping/calc_mapping_times.py
@@ -32,6 +32,6 @@ for b in xrange(4, 300, 4):
               if mins[b] > time:
                 mins[b] = time
 
-print "sc_count, avg_running_time, min_running_time, max_running_time"
+print "sc_count, avg_running_time, min_running_time, max_running_time, sample_size"
 for b in map_times:
-  print "%s, %s, %s, %s"%b, map_times[b]/cnts[b], mins[b], maxs[b]
+  print "%s, %s, %s, %s"%b, map_times[b]/cnts[b], mins[b], maxs[b], cnts[b]
