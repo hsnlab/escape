f738626e3d3d46893001bcf43edb65cf51aaf1bc
Janos Czentye <czentye@tmit.bme.hu>
2016-08-18 12:50:14 +0200

    Improve and unify startup script for both lo/mdo ESCAPE

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 # Init script for docker container
-sudo service ssh start
 sudo service neo4j-service start
-sudo service openvswitch-switch start
-/bin/bash
+
+# Skip in case of ESCAPE MdO
+if [ -f /etc/init.d/ssh ]; then
+    sudo service ssh start
+fi
+
+# Skip in case of ESCAPE MdO
+if [ -f /etc/init.d/openvswitch-switch ]; then
+    sudo service openvswitch-switch start
+fi
