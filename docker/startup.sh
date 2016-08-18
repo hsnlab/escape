f59e40dd78fafd0229658439b7bbd15ae9feb981
Janos Czentye <czentye@tmit.bme.hu>
2016-08-18 15:46:30 +0200

    Fix service detection in startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,21 +4,21 @@
 echo -e "Check necessary services...\n"
 
 if [ -f /etc/init.d/neo4j-service ]; then
-    if pgrep -af neo4j; then
+    if ! pgrep -af neo4j; then
         sudo service neo4j-service start
     fi
 fi
 
 # Skip in case of ESCAPE MdO
 if [ -f /etc/init.d/ssh ]; then
-    if pgrep -af sshd; then
+    if ! pgrep -af sshd; then
         sudo service ssh start
     fi
 fi
 
 # Skip in case of ESCAPE MdO
 if [ -f /etc/init.d/openvswitch-switch ]; then
-    if pgrep -af ovs-vswitchd; then
+    if ! pgrep -af ovs-vswitchd; then
         sudo service openvswitch-switch start
     fi
 fi
