36650754a442d2714119fc65ccae1e01e7ad9686
Janos Czentye <czentye@tmit.bme.hu>
2016-08-18 15:25:15 +0200

    Improve startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 # Init script for docker container
-sudo service neo4j-service start
+echo -e "Check necessary services...\n"
+
+if [ -f /etc/init.d/neo4j-service ]; then
+    if pgrep -af neo4j; then
+        sudo service neo4j-service start
+    fi
+fi
 
 # Skip in case of ESCAPE MdO
 if [ -f /etc/init.d/ssh ]; then
-    sudo service ssh start
+    if pgrep -af sshd; then
+        sudo service ssh start
+    fi
 fi
 
 # Skip in case of ESCAPE MdO
 if [ -f /etc/init.d/openvswitch-switch ]; then
-    sudo service openvswitch-switch start
+    if pgrep -af ovs-vswitchd; then
+        sudo service openvswitch-switch start
+    fi
 fi
