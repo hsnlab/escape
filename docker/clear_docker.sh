294c6d2b0a7886b64999fe17e934f08cfa8acd57
Janos Czentye <czentye@tmit.bme.hu>
2016-08-22 12:36:41 +0200

    Generalize clear_docker.sh

--- docker/clear_docker.sh
+++ docker/clear_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clear containers
-docker rm $(docker ps --no-trunc -aq)
+sudo docker rm $(docker ps --no-trunc -aq)
 # Clear imaged
-docker rmi $(docker images -q -f "dangling=true")
-docker rmi escape/mdo
+sudo docker rmi $(docker images -q -f "dangling=true")
+sudo docker rmi escape/mdo
