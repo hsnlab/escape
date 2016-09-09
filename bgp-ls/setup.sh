39e6e8bff619a01ffec04928513345400cc1c0a0
Janos Czentye <czentye@tmit.bme.hu>
2016-09-09 11:28:27 +0200

    Add netphony-topology as submodule Tweak setup.sh init script Add new tesing config files for BGP-LS Add dummy-orchestrator as submodule for testing Update netphony-topology to get bugfixes

--- bgp-ls/setup.sh
+++ bgp-ls/setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt-get install maven
-git clone https://github.com/jgrajos/netphony-network-protocols.git
-cd netphony-network-protocols
-git checkout feature/BGPLS-IT-v2
-mvn install
-cd ..
-#git clone git@5gexgit.tmit.bme.hu:ogd/netphony-topology.git
-git submodule update --recursive
+if [ ! -d netphony-network-protocols ]; then
+    pushd
+    sudo apt-get update && sudo apt-get install maven
+    git clone https://github.com/jgrajos/netphony-network-protocols.git
+    cd netphony-network-protocols
+    git checkout feature/BGPLS-IT-v2
+    mvn install
+    popd
+fi
+git submodule update --init --recursive --merge --remote
 cd netphony-topology
 mvn clean package -P generate-full-jar -P bgp-ls-speaker assembly:single
