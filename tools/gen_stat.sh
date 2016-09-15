4d7b8afd5a22f0dc006e122b832642b0df54c08d
Janos Czentye <czentye@tmit.bme.hu>
2016-09-15 14:17:37 +0200

    Update dummy-orchestrator Add newest version of clock

--- tools/gen_stat.sh
+++ tools/gen_stat.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-cloc --unicode --exclude-ext=config,xml,json --exclude-dir=mininet,pox,OpenYuma,bgp-ls/netphony-topology,escape/doc  .. 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$( dirname "$DIR" )
+echo "Project dir: $PROJECT_DIR"
+#cloc --unicode --exclude-ext=config,xml,json --exclude-dir=mininet,pox,OpenYuma,bgp-ls/netphony-topology,escape/doc  .. 
+${DIR}/cloc-1.70.pl --unicode --exclude-ext=config,xml,json \
+    --exclude-dir=mininet,pox,OpenYuma,netphony-topology,build \
+    --not-match-f=cloc-.*.pl ${PROJECT_DIR}
