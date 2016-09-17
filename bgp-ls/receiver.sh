a07b5eded7030506f59f21c13f941169b1fa339e
Janos Czentye <czentye@tmit.bme.hu>
2016-09-17 15:28:19 +0200

    Add test files,configs to BGP-LS speaker

--- bgp-ls/receiver.sh
+++ bgp-ls/receiver.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-ROOT=$PWD
-cd netphony-topology
 if [ "$#" -eq 0 ]; then
-	java -Dlog4j.configurationFile=log4j2.xml \
-	    -jar target/topology-1.3.2-shaded.jar \
-	    ${ROOT}/TMConfig_esc.xml
+	java -Dlog4j.configurationFile=log4j2.xml -jar topology-1.3.2-shaded.jar receiver-TMConfig_esc.xml
 else
-	java -Dlog4j.configurationFile=log4j2.xml -jar target/topology-1.3.2-shaded.jar $@
+	java -Dlog4j.configurationFile=log4j2.xml -jar topology-1.3.2-shaded.jar $@
 fi
