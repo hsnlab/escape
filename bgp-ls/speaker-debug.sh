22128753495d0f114b2c6c775cf7e68ae957cfa2
Janos Czentye <czentye@tmit.bme.hu>
2016-09-28 11:57:27 +0200

    Set back to main 5gex branch in bgp-ls submodule

--- bgp-ls/speaker-debug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$#" -eq 0 ]; then
-	java -Dlog4j.configurationFile=log4j2-debug.xml -jar bgp-ls-speaker-jar-with-dependencies.jar speaker-BGP4Parameters_1_esc.xml
-else
-	java -Dlog4j.configurationFile=log4j2-debug.xml -jar bgp-ls-speaker-jar-with-dependencies.jar $@
-fi
