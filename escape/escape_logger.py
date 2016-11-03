b8a18f5ac0da34674a65064e6cc7aadb00bc7f27
Janos Czentye <czentye@tmit.bme.hu>
2016-11-03 12:37:39 +0100

    Improve logger to log into a file in test mode

--- escape/escape_logger.py
+++ escape/escape_logger.py
@@ -19,24 +19,62 @@ Based on pretty_logger: /pox/pox/samples/pretty_log.py
 import logging
 
 import pox.log
-from pox.log import color, level
 from pox.core import log
+from pox.log import color, level
 
 # Add new VERBOSE log level to root logger
 logging.addLevelName(5, 'VERBOSE')
 logging.getLogger('').setLevel("VERBOSE")
 
+# Set color for the additional VERBOSE level in POX
 color.LEVEL_COLORS['VERBOSE'] = 'white'
 
-DEFAULT_FORMAT = "[@@@bold@@@level%(name)-23s@@@reset] @@@bold%(" \
-                 "message)s@@@normal"
-TEST_FORMAT = "[@@@bold@@@level%(levelname)-18s@@@reset][@@@bold@@@level%(" \
-              "name)-23s@@@reset] @@@bold%(message)s@@@normal"
+# Use logger format from pretty_logger
+DEFAULT_LOGGER_FORMAT = "[@@@bold@@@level%(name)-23s@@@reset] " \
+                        "@@@bold%(message)s@@@normal"
+# Add logger level to the log entries in test mode
+TEST_LOGGER_FORMAT = "[@@@bold@@@level%(levelname)-18s@@@reset]" \
+                     "[@@@bold@@@level%(name)-23s@@@reset]" \
+                     " @@@bold%(message)s@@@normal"
+# Add logger level to the log entries in test mode
+FILE_LOGGER_FORMAT = "|%(levelname)s" \
+                     "|%(name)s" \
+                     "|%(asctime)s" \
+                     "| %(message)s"
+# Log file name
+LOG_FILE = "escape.log"
 
 
 def launch (test_mode=False, **kw):
-  pox.log.launch(format=TEST_FORMAT if test_mode else DEFAULT_FORMAT)
-  color.launch()
+  """
+  Launch and set parameters for logging.
+
+  :param test_mode: use test mode logging (default: False)
+  :type test_mode: bool
+  :param kw: additional parameters for POX's logger
+  :type kw: dict
+  :return: None
+  """
+  # Enable logging in specific logging level
   level.launch(**kw)
-  log.info("Setup logger - formatter: %s, level: %s" % (
-    launch.__module__, logging.getLevelName(log.getEffectiveLevel())))
+  # Launch colorful logging
+  color.launch()
+  if test_mode:
+    # Define logger for test mode
+    pox.log.launch(format=TEST_LOGGER_FORMAT)
+    log.info("Setup Logger - formatter: %s, level: %s"
+             % (pox.log.launch.__module__,
+                logging.getLevelName(log.getEffectiveLevel())))
+
+    # Define additional logger for logging to file
+    pox.log.launch(format=FILE_LOGGER_FORMAT, file=LOG_FILE + ',w')
+    log.info("Setup Logger - formatter: %s, level: %s, file: %s"
+             % (pox.log.launch.__module__,
+                logging.getLevelName(log.getEffectiveLevel()),
+                LOG_FILE))
+  else:
+    # Define default logger
+    pox.log.launch(format=DEFAULT_LOGGER_FORMAT)
+    log.info("Setup logger - formatter: %s, level: %s"
+             % (launch.__module__,
+                logging.getLevelName(log.getEffectiveLevel())))
