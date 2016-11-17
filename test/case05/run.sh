67aa3c53466a42eb63ff4a98b00468ad91d093f8
Janos Czentye <czentye@tmit.bme.hu>
2016-11-17 18:51:15 +0100

    Add test case 5 Add domain merge improvement

---
+++ test/case05/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\n======================================================================"
+echo -e "==                        TEST CASE 5                               =="
+echo -e "======================================================================\n"
+
+./escape.py --debug --test --quit --log test/case05/escape.log --config test/case05/test.config --service test/case05/request.nffg
