ddbdf605c0b11c4db08403069a861903af6963ca
Janos Czentye <czentye@tmit.bme.hu>
2016-11-17 15:21:39 +0100

    Remove wrong files

--- test/case2/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n======================================================================"
-echo -e "==                        TEST CASE 2                               =="
-echo -e "======================================================================\n"
-
-sudo ./escape.py --debug --test --quit --log test/case2/escape.log --full --config test/case2/test.config --service test/case2/request.nffg
--- test/case3/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n======================================================================"
-echo -e "==                        TEST CASE 3                               =="
-echo -e "======================================================================\n"
-
-./escape.py --debug --test --quit --log test/case3/escape.log --config test/case3/test.config --service test/case3/request.nffg
