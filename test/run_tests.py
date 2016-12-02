2d6448de03494709cf0d17023806f0b827bf2913
Janos Czentye <czentye@tmit.bme.hu>
2016-12-02 15:57:41 +0100

    Move obsolete runner script under tools dir Add credits to py files

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2015 Lajos Gerecs
+# Copyright 2015 Lajos Gerecs, Janos Czentye
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
