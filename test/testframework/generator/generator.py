91fec7cdcfe0d0b463d206ee01fcca4a4132ad41
Janos Czentye <czentye@tmit.bme.hu>
2016-12-16 22:01:35 +0100

    Extract default seed value ot global var

--- test/testframework/generator/generator.py
+++ test/testframework/generator/generator.py
@@ -12,21 +12,25 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with POX. If not, see <http://www.gnu.org/licenses/>.
+import logging
 from functools import partial
 
 import e2e_reqs_for_testframework
 import networkx_nffg_generator as nrg
 import sg_generator
 
+logging.disable(logging.WARNING)
+DEFAULT_SEED = 0
+
 eight_loop_requests = partial(sg_generator.get_8loop_request,
                               abc_nf_types_len=10,
-                              seed=0,
+                              seed=DEFAULT_SEED,
                               eightloops=1)
 
 complex_e2e_reqs = partial(e2e_reqs_for_testframework.main,
                            loops=False,
                            vnf_sharing=0.0,
-                           seed=0,
+                           seed=DEFAULT_SEED,
                            multiple_scs=False,
                            use_saps_once=False,
                            max_sc_count=2,
