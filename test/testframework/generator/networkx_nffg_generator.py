8fad6e81b5ed221f1174f400454b14cf2da0ded1
Balazs Nemeth <balazs.nemeth@tmit.bme.hu>
2016-12-20 18:27:34 +0100

    add graph generation based on NetworkX functions

--- test/testframework/generator/networkx_nffg_generator.py
+++ test/testframework/generator/networkx_nffg_generator.py
@@ -22,7 +22,7 @@ import random
 import string
 import sys
 
-import networkx as nx
+from sg_generator import getName
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              "../../../escape/escape/nffg_lib/")))
@@ -34,33 +34,75 @@ def get_networkx_func (func_name, seed=0, **kwargs):
   Uses 'func_name' graph generator of NetworkX library to create a NetworkX 
   graph which can be used as topology.
   """
-  nx_func = getattr(importlib.import_module(nx), func_name)
+  nx_func = getattr(importlib.import_module("networkx"), func_name)
   generated_graph = nx_func(seed=seed, **kwargs)
   return generated_graph
 
 
-def networkx_request_generator (func_name, seed=0, max_cpu=4, max_mem=1600,
-                                max_storage=3, max_bw=7, abc_nf_types_len=10,
-                                **kwargs):
+def networkx_resource_generator (func_name, seed=0, max_cpu=4, max_mem=1600,
+                                 max_storage=3, max_link_bw=7,
+                                 abc_nf_types_len=10,
+                                 supported_nf_cnt=6, max_link_delay=2,
+                                 sap_cnt=10,
+                                 **kwargs):
   """
   Uses a NetworkX graph to create a request NFFG.
+
+  :param func_name: string of NetworkX lib's random graph generator function
+  name
+  :param seed:
+  :param max_cpu:
+  :param max_mem:
+  :param max_storage:
+  :param max_link_bw:
+  :param abc_nf_types_len:
+  :param supported_nf_cnt:
+  :param max_link_delay:
+  :param sap_cnt:
+  :param kwargs:
+  :return:
   """
   rnd = random.Random()
   rnd.seed(seed)
   nx_graph = get_networkx_func(func_name, seed=seed, **kwargs)
 
   nf_types = list(string.ascii_uppercase)[:abc_nf_types_len]
-
   nffg = NFFG(id="req-" + func_name + "-seed" + str(seed))
   nffg.mode = NFFG.MODE_ADD
-  for nf_id in nx_graph.nodes_iter():
-    nf = nffg.add_nf(id=nf_id,
-                     func_type=rnd.choice(nf_types),
-                     cpu=rnd.random() * max_cpu,
-                     mem=rnd.random() * max_mem,
-                     storage=rnd.random() * max_storage)
 
-  for i, j, k in nx_graph.edges_iter(keys=True):
-    pass
+  for infra_id in nx_graph.nodes_iter():
+    infra = nffg.add_infra(id=infra_id,
+                           bandwidth=rnd.random() * max_link_bw * 1000,
+                           cpu=rnd.random() * max_cpu,
+                           mem=rnd.random() * max_mem,
+                           storage=rnd.random() * max_storage)
+    infra.add_supported_type(rnd.sample(nf_types, supported_nf_cnt))
+
+  for i, j in nx_graph.edges_iter():
+    infra1 = nffg.network.node[i]
+    infra2 = nffg.network.node[j]
+    nffg.add_undirected_link(port1=infra1.add_port(id=getName("port")),
+                             port2=infra2.add_port(id=getName("port")),
+                             p1p2id=getName("link"), p2p1id=getName("link"),
+                             dynamic=False,
+                             delay=rnd.random() * max_link_delay,
+                             bandwidth=rnd.random() * max_link_bw)
+
+  infra_ids = [i.id for i in nffg.infras]
+  for s in xrange(0, sap_cnt):
+    sap_obj = nffg.add_sap(id=getName("sap"))
+    sap_port = sap_obj.add_port(id=getName("port"))
+    infra_id = rnd.choice(infra_ids)
+    infra = nffg.network.node[infra_id]
+    nffg.add_undirected_link(port1=sap_port,
+                             port2=infra.add_port(id=getName("port")),
+                             p1p2id=getName("link"), p2p1id=getName("link"),
+                             dynamic=False,
+                             delay=rnd.random() * max_link_delay,
+                             bandwidth=rnd.random() * max_link_bw)
 
   return nffg
+
+if __name__ == "__main__":
+  print networkx_resource_generator("erdos_renyi_graph", seed=5, n=6, p=0.3, sap_cnt=15).dump()
+
