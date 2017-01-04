6e58cea1e9971d97e5abb651e05b1487c22f9478
Janos Czentye <czentye@tmit.bme.hu>
2017-01-04 12:44:05 +0100

    Rename case18 to case20

--- test/case20/run.sh
+++ test/case20/run.sh
@@ -14,19 +14,13 @@ echo -e "=======================================================================
 
 # Print test case description
 cat ${CWD}/README.txt
-echo -e "\n===============================================================================\n"
-echo
-## Test case header - END
-
-# Print test case description
-cat ${CWD}/README.txt
 echo -e "\n=============================== START TEST CASE ===============================\n"
 echo
 ## Test case header - END
 
 # Define run command here
-ESCAPE_CMD="${ESCAPE} --debug --test --quit --log ${CWD}/escape.log --config \
-            ${CWD}/test.config --service ${CWD}/gen-request.nffg"
+ESCAPE_CMD="${ESCAPE} --debug --test --log ${CWD}/escape.log \
+            --config ${CWD}/test.config"
 
 # Invoke ESCAPE with test parameters
 time ${ESCAPE_CMD} $@
