b6ab933512b76a01a10be017f7212359835581fc
Janos Czentye <czentye@tmit.bme.hu>
2017-03-31 12:19:04 +0200

    Add support of Alpine Linux in install_requirements.sh for testing

--- test/install_requirements.sh
+++ test/install_requirements.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
-apt-get install sudo bash net-tools
+SHELL_DEPS="sudo net-tools"
+
+if type apt >> /dev/null; then
+    sudo apt-get install -y ${SHELL_DEPS}
+elif type apk >> /dev/null; then
+    apk add ${SHELL_DEPS}
+fi
 sudo -H pip install pexpect unittest-xml-reporting pyyaml requests
