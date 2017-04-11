22f9a897f4ea38ae1357ca5610bb080ca36ab161
Janos Czentye <czentye@tmit.bme.hu>
2017-04-11 14:28:39 +0200

    Fix kill-escape.sh

--- tools/kill-escape.sh
+++ tools/kill-escape.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Kill running ESC process(es) and clear remained subprocesses/files/infs
 
-pgrep -af ESCAPE
+pgrep -af escape.py
 # -S read password from STDIN - but it will be shown on console in PyCharm!
-sudo -S pkill -f ESCAPE
+sudo -S pkill -f escape.py
 sudo -S ./escape.py -x
