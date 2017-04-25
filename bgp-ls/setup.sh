a9a96f14d1f251a7a3a4ed4caf18156c3d9d8f80
Janos Czentye <czentye@tmit.bme.hu>
2017-04-25 18:29:56 +0200

    Remove unnecessary config files used for TADS

--- bgp-ls/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update && sudo apt-get install maven
-git submodule update --init --recursive --merge --remote
-cd netphony-network-protocols
-mvn install
-cd ../netphony-topology
-#mvn clean package -P generate-full-jar -P bgp-ls-speaker assembly:single
-mvn clean package -P generate-full-jar
