d2d7d312cf773a18819aa19ac4ac4819986b6ab2
Janos Czentye <czentye@tmit.bme.hu>
2017-04-26 16:24:21 +0200

    Improve gen_stat.sh to download sloc counting script automatically

--- tools/gen_stat.sh
+++ tools/gen_stat.sh
@@ -6,8 +6,12 @@ echo "Project dir: $PROJECT_DIR"
 CLOC="$DIR/cloc-1.72.pl"
 if [ ! -f ${CLOC} ]; then
     echo "Missing script: $CLOC --> Download from https://github.com/AlDanial/cloc"
-    exit -1
+    cd ${DIR}
+    wget https://github.com/AlDanial/cloc/releases/download/v1.72/cloc-1.72.pl
+    chmod a+x ${CLOC}
+    cd -
 fi
+echo "Source code stat:"
 ${CLOC} --unicode --exclude-ext=config,xml,json,nffg \
     --exclude-dir=mininet,pox,OpenYuma,netphony-topology,build \
     --not-match-f=cloc-.*.pl ${PROJECT_DIR}
