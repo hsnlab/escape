ae0156b511520a0f5954469fe0323bd939b9258b
Janos Czentye <czentye@tmit.bme.hu>
2017-05-12 16:29:58 +0200

    Add testcase28 for allowed_host constraint testing Add testcase29 for new consumer/provider feature testing

---
+++ test/case28/run.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+## Test case header - START
+# Get directory path of current test case
+CWD="$(dirname $(readlink -f "$0"))"
+# Get test case name
+TEST_CASE="$( basename ${CWD} | tr '[:lower:]' '[:upper:]' )"
+# Get ESCAPE command
+ESCAPE="$( readlink -f ${CWD}/../../escape.py )"
+if which time >> /dev/null; then
+    RUN_WITH_MEASUREMENT="$(which time) -v"
+elif which bash >> /dev/null; then
+    RUN_WITH_MEASUREMENT=""
+fi
+# Print header
+echo
+echo "==============================================================================="
+echo "==                             TEST $TEST_CASE                                   =="
+echo "==============================================================================="
+echo
+# Print test case description
+cat ${CWD}/README.txt
+echo
+echo "=============================== START TEST CASE ==============================="
+echo
+## Test case header - END
+
+# Define run command here
+ESCAPE_CMD="${ESCAPE} --debug --test --quit --log ${CWD}/escape.log \
+            --config ${CWD}/test.config --service ${CWD}/escape-mn-req-allowednodes.nffg"
+
+# Invoke ESCAPE with test parameters
+${RUN_WITH_MEASUREMENT} ${ESCAPE_CMD} $@
+
+## Test case footer - START
+echo
+echo "===================================== END ====================================="
+echo
+## Test case footer - END
---
+++ test/case29/README.txt
@@ -0,0 +1,5 @@
+Infrastructure:     1 global DoV managed by NFFG-based StaticFileAdapters
+Topology:           2 merged ESCAPE domain
+Request:            Robot request with PID  helper and splitter
+Mapping:            1 layer mapping
+Expected result:    SUCCESS
