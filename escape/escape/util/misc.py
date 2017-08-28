6e2b9159f598cbc3b0e92a4e39e839160f4c5c5b
Janos Czentye <czentye@tmit.bme.hu>
2017-08-28 18:25:12 +0200

    Imporve Singleton metaclass to accept keywords in constructor

--- escape/escape/util/misc.py
+++ escape/escape/util/misc.py
@@ -304,12 +304,12 @@ class Singleton(type):
   """
   _instances = {}
 
-  def __call__ (cls, *args):
+  def __call__ (cls, *args, **kwargs):
     """
     Override.
     """
     if cls not in cls._instances:
-      cls._instances[cls] = super(Singleton, cls).__call__(*args)
+      cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
     return cls._instances[cls]
 
 
