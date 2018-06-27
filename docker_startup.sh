a897014120c09b3fc4d5ff2df7bf8ae2bc9a8d94
Janos Czentye <czentye@tmit.bme.hu>
2018-06-27 13:05:56 +0200

    First steps toslicer integration

--- docker_startup.sh
+++ docker_startup.sh
@@ -19,11 +19,21 @@ STOP_VALUE=242
 
 trap term_handler INT TERM STOP
 
+SLICER_PORT=8888
+ESCAPE_API_PORT=9999
+USE_SLICER="False"
+SLICE_CFG_FOLDER="../slices/"
+
 ESCAPE_PID=""
+SLICER_PID=""
 
 function term_handler() {
     echo "Terminating ESCAPE process: $ESCAPE_PID"
     kill -SIGTERM ${ESCAPE_PID}
+    if [ -n ${SLICER_PID} ]; then
+        echo "Terminating SLICER process: $SLICER_PID"
+        kill -SIGINT ${SLICER_PID}
+    fi
 }
 
 function update() {
@@ -38,9 +48,18 @@ echo -e "152.66.246.230\t5gexgit.tmit.bme.hu" >> /etc/hosts
 while true
 do
     echo "Received params: $@"
+    echo "Starting ESCAPE..."
     python escape.py ${@} &
     ESCAPE_PID=${!}
     echo "ESCAPE PID: $ESCAPE_PID"
+    if [ ${USE_SLICER:-False} != "False" ]; then
+        echo "Starting Slicer..."
+        python ./slicer_bin/slicer.pyc --port ${SLICER_PORT} --host 0.0.0.0 \
+            --mdo "http://127.0.0.1:$ESCAPE_API_PORT/escape/orchestration/" \
+            -ds v0 &
+        SLICER_PID=${!}
+        echo "SLICER PID: $SLICER_PID"
+    fi
     wait ${ESCAPE_PID}
     ret_value=${?}
     echo "Received exit code from ESCAPE: $ret_value"
